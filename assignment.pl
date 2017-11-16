6b71e9409cdfc2594757c06ffad90709cb4054ed
AisCar <aislingcarrick@yahoo.ie>
2017-11-16 15:16:32 +0000

    Add files via upload
    Programming Paradigms Assignment 4 Submission

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,23 @@
 %Definitions for isElementInList(El, List)
+isElementInList(X,X).
+isElementInList(X,[X|_]).
+isElementInList(X,[_|T]):-
+    isElementInList(X,T).
 
 %Definitions for reverseList(List, ReversedList)
+reverseList([],[]).
+reverseList([H|T], R):-
+    reverseList(T,A),
+    mergeLists(A,[H],R).
 
 %insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(El, [], [El]).
+insertElementIntoListEnd(El, [H|T], [H|NewList]):-
+    insertElementIntoListEnd(El, T, NewList).
+
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([], Y, Y).
+mergeLists([H|T],Y,X):-
+    mergeLists(T, Y, A),
+    X = [H|A].
